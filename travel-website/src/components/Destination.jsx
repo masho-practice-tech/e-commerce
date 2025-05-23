467e45d0a02b4728f82a5ee7548f037239a8df10
masho-practice-tech <masho.practice2@gmail.com>
2025-05-23 05:44:07 +0000

    bg-video is changed

--- travel-website/src/components/Destination.jsx
+++ travel-website/src/components/Destination.jsx
@@ -6,7 +6,7 @@ const Destination = () => {
  return (
           <section id="destinations" className="max-w-7xl mx-auto px-6 py-20">
             <h2 data-aos='zoom-in' className="text-4xl font-extrabold text-center mb-12">Popular Destinations</h2>
-            <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4 ">
               {destinations.map((destination) => (
                 <DestinationCard
                   key={destination.title} destination={destination} />
--- travel-website/src/components/DestinationCard.jsx
+++ travel-website/src/components/DestinationCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const DestinationCard = ({destination}) => {
  return (
-          <div data-aos='fade-up' className="rounded-lg overflow-hidden shadow-lg bg-gray-800 hover:scale-105 transform transition duration-300">
-            <img src={destination.image} alt={destination.title} className="w-full h-48 object-cover" loading="lazy" />
+          <div data-aos='fade-up' className=" rounded-lg overflow-hidden shadow-lg bg-gray-800  ">
+            <img src={destination.image} alt={destination.title} className="w-full h-48 object-cover " loading="lazy" />
             <div  className="p-4">
               <h3 className="text-xl font-bold mb-2">{destination.title}</h3>
               <p className="text-gray-300 text-sm">{destination.description}</p>
